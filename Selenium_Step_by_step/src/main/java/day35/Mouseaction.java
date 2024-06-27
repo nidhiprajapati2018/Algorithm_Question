fa829ac30af742d5189e9fc5f4709827da1bf5f7
Nidhi <nidhi.khurpia@gmail.com>
2024-06-27 13:50:00 -0700

    Selenium_topics

---
+++ Selenium_Step_by_step/src/main/java/day35/Mouseaction.java
@@ -0,0 +1,30 @@
+package day35;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Mouseaction {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demo.opencart.com/");
+		driver.manage().window().maximize();
+		
+		WebElement desktop = driver.findElement(By.xpath("//a[normalize-space()='Desktops']"));
+		WebElement mac = driver.findElement(By.xpath("//a[normalize-space()='Mac (1)']"));
+		
+		Actions act = new Actions(driver);
+		
+		//mouse hover
+		//act.moveToElement(desktop).moveToElement(mac).build().perform();
+		act.moveToElement(desktop).moveToElement(mac).perform();
+
+	}
+
+}
